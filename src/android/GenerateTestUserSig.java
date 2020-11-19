5369158d9b3f98e9d63ae974811b2014985192be
antugar <shuigui8988@sina.com>
2020-11-20 05:46:12 +0800

    delete

--- src/android/GenerateTestUserSig.java
+++ src/android/GenerateTestUserSig.java
@@ -42,7 +42,7 @@ public class GenerateTestUserSig {
      * 进入腾讯云实时音视频[控制台](https://console.cloud.tencent.com/rav ) 创建应用，即可看到 SDKAppId，
      * 它是腾讯云用于区分客户的唯一标识。
      */
-    public static final int SDKAPPID = 1400447762;
+    public static final int SDKAPPID = 0;
 
 
     /**
@@ -64,7 +64,7 @@ public class GenerateTestUserSig {
      * 注意：该方案仅适用于调试Demo，正式上线前请将 UserSig 计算代码和密钥迁移到您的后台服务器上，以避免加密密钥泄露导致的流量盗用。
      * 文档：https://cloud.tencent.com/document/product/647/17275#Server
      */
-    public static final String SECRETKEY = "3d860dc15ef9461f31c580b1b814f96003ff90a2b8555e08e148228b189978e9";
+    public static final String SECRETKEY = "";
     private static final byte plussym = "+".getBytes()[0];
     private static final byte strisym = "/".getBytes()[0];
     private static final byte undersym = "=".getBytes()[0];
